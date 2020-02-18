de3d5e0b23b4469bcdb02991c0efe38cfadd801a
TanCK97 <ctan0017@student.monash.edu>
2020-02-18 17:41:02 +1100

    Completed

--- end_to_end_net.py
+++ end_to_end_net.py
@@ -9,6 +9,10 @@ import torchvision.transforms as transforms
 import torchvision.models as models
 from torch.utils.data import Dataset, DataLoader
 import random
+import torch.nn as nn
+import torch.optim as optim
+
+EPOCHS = 10
 
 """
 DATALOADER
@@ -16,15 +20,20 @@ DATALOADER
 class HandoverDataset(Dataset):
     """Handover dataset"""
 
-    def __init__(self, csv_file, img_dir, transform=None):
+    def __init__(self, csv_file, img_dir, transform=None, split='train'):
         """
         Args:
             csv_file (string): Path to the csv file with annotations.
             img_dir (string): Directory with all the images.
             transform (callable, optional): Optional transform to be applied
                 on a sample.
+            split (string): Split the dataset into training or testing set
         """
-        self.handover = pd.read_csv('datasets/Handover/classes.csv')
+
+        if split == 'train':
+            self.handover = pd.read_csv('datasets/Handover/classes_train.csv')
+        elif split == 'test':
+            self.handover = pd.read_csv('datasets/Handover/classes_test.csv')
         self.handover_dict = self.handover.to_dict("records")
         self.img_dir = img_dir
         self.transform = transform
@@ -42,31 +51,91 @@ class HandoverDataset(Dataset):
         sample = {"image": image, "class":cls}
 
         if self.transform:
-            sample = self.transform(sample)
+            sample["image"] = self.transform(sample["image"])
 
         return sample
 
-
-handover_dataset = HandoverDataset(csv_file='datasets/Handover/classes.csv',
-                                    img_dir='datasets/Handover/handover')
-
-# fig = plt.figure()
-#
-# for i in random.sample(range(0,len(handover_dataset)), 4):
-#     sample = handover_dataset[i]
-#
-#     print(i, type(sample['image']), sample['class'])
-#
-#     plt.imshow(sample['image'])
-#     plt.show()
-#     plt.figure()
-
 transform = transforms.Compose([transforms.ToPILImage(),
                                 transforms.RandomHorizontalFlip(),
-                                transforms.RandomResizedCrop(handover_dataset[0]['image'].shape[0:1], scale=(0.7,1.0)),
+                                transforms.RandomResizedCrop((720, 1280), scale=(0.7,1.0)),
                                 transforms.ToTensor()])
+
+handover_train = HandoverDataset(csv_file='datasets/Handover/classes_train.csv',
+                                    img_dir='datasets/Handover/handover',
+                                    transform=transform,
+                                    split='train')
+
+# for i in range(len(handover_train)):
+#     sample = handover_train[i]
+#
+#     print(i, sample['image'].size(), sample['class'])
+#
+#     if i == 3:
+#         break
+
+transform = transforms.Compose([transforms.ToTensor()])
+
+handover_test = HandoverDataset(csv_file='datasets/Handover/classes_test.csv',
+                                    img_dir='datasets/Handover/handover',
+                                    transform=transform,
+                                    split='test')
+
+trainloader = DataLoader(handover_train, batch_size=2, shuffle=True, num_workers=2)
+testloader = DataLoader(handover_test, batch_size=1, shuffle=False, num_workers=2)
 
 """
 MODEL
 """
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# print(device)
+
 resnet50 = models.resnet50()
+resnet50 = resnet50.to(device)
+# print(resnet50)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(resnet50.parameters(), lr=0.0001)
+
+"""
+TRAINING LOOP
+"""
+for epoch in range(EPOCHS):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a dict of [inputs, class]
+        inputs, cls = data['image'].to(device), data['class'].to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = resnet50(inputs)
+        loss = criterion(outputs, cls)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 10 == 9:    # print every 2000 mini-batches
+            print('[%d, %3d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 10))
+            running_loss = 0.0
+
+print('Finished Training')
+
+"""
+TESTING LOOP
+"""
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, cls = data['image'].to(device), data['class'].to(device)
+        outputs = resnet50(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += cls.size(0)
+        correct += (predicted == cls).sum().item()
+
+print('Accuracy of the network on the %d test images: %d %%' % (len(testloader),
+    100 * correct / total))
